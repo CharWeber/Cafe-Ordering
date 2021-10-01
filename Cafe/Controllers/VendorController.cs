b8c29ee67c4f381b3e3f59f02434cbcdc48c54e0
Charlie Weber <charlestweber@gmail.com>
2021-10-01 13:52:21 -0700

    flushed out front end, vendors can see order details and add new orders

--- Cafe/Controllers/VendorController.cs
+++ Cafe/Controllers/VendorController.cs
@@ -12,18 +12,20 @@ namespace Cafe.Controllers
       List <Vendor> AllVendors = Vendor.GetAll();
       return View(AllVendors);
     }
+
     [HttpGet("/vendors/new")]
     public ActionResult New()
     {
       return View();
     }
+
     [HttpPost("/vendors")]
     public ActionResult Create(string vendorName, string vendorDescription)
     {
       Vendor newVendor = new Vendor(vendorName, vendorDescription);
-
       return RedirectToAction("Index");
     }
+
     [HttpGet("/vendors/{Id}")]
     public ActionResult Show(int Id)
     {
@@ -33,6 +35,19 @@ namespace Cafe.Controllers
       model.Add("vendor", selectedVendor);
       model.Add("orders", vendorOrders);
       return View(model);
+    }
+
+    [HttpPost("/vendors/{Id}/orders")]
+    public ActionResult Create(int Id, string description, int qty, int price, string date)
+    {
+      Dictionary<string, object> model = new Dictionary <string, object>();
+      Vendor selectedVendor = Vendor.Find(Id);
+      Order newOrder = new Order(qty, description, price, date);
+      selectedVendor.AddOrder(newOrder);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrders);
+      return View("Show", model);
     }
   }
 }
